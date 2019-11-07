13ca30e9ac8a481bc7083c28290711bf4125d759
JohnKHancock <jkhancock@gmail.com>
2019-11-06 21:02:59 -0500

    2019-11-06

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+function reverseIt(){
+	var inputString = document.getElementById("inputTextString").value;
+    var splitString = inputString.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+	str = "The reverse of the text that you entered: " + joinArray;
+    document.getElementById("output").innerHTML = str;
+}
+
+function multiples(){
+	var inputNum = document.getElementById("inputNumString").value;
+	str="<table><tr>"
+
+	for (i=1; i<=20; i++)
+	{
+		if (i%4==0)
+		{
+			str = "</tr>"+ str + "<td>" + inputNum*i.toString() + "</td>"+ "<tr>"
+		}else{
+			str =  str + "<td>" + inputNum*i.toString() + "</td>"
+			}
+	}
+
+	str + "</tr></table>"
+
+    document.getElementById("output2").innerHTML = str;
+}
+
+
 var tabulate = function (data,columns) {
-		  var table = d3.select('body').append('table')
+		  var table = d3.select('#output3').append('table')
 			var thead = table.append('thead')
 			var tbody = table.append('tbody')
 
@@ -24,13 +53,17 @@ var tabulate = function (data,columns) {
 		      .enter()
 		    .append('td')
 		      .text(function (d) { return d.value })
-
+			table.attr("id","presidents")
 		  return table;
 	    }
 
 
-            d3.csv('presidents.csv', function(data)
+        d3.csv('presidents.csv', function(data)
                 {
-                    var columns = ["Name", "Height", "Weight"]
-                    tabulate(data,columns)
+                   var columns = ["Name", "Height", "Weight"]
+                   tabulate(data,columns)
                  })
+
+
+
+
